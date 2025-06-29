c0bc4845128d1ea399bb7d9102aa2582a66457c6
daenikon <dkondratiuk06@gmail.com>
2025-06-29 10:51:43 +0200

    add login form

--- frontend/src/components/authentication/signup-form.tsx
+++ frontend/src/components/authentication/signup-form.tsx
@@ -67,8 +67,8 @@ export default function SingupForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Already have an account?{" "}
-              <a href="/signin" className="underline underline-offset-4">
-                Sign in
+              <a href="/login" className="underline underline-offset-4">
+                Login
               </a>
             </div>
           </form>
